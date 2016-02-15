9ab41d753514094dc9f4108d8a56bd78aea3cb75
Josh Dillon <joshdillon29@gmail.com>
2016-02-15 17:36:02 -0500

    Added my code to server and ui

--- app/josh_work.R
+++ app/josh_work.R
@@ -1,19 +1,54 @@
-# Set up workspace
-setwd("/Users/Josh/Documents/Spring 2016/DataScience/Project2/project2-cycle2-8/")
-library(shiny)
-library(dplyr)
-library(data.table)
-library(leaflet)
+library(plotly)
+library(zoo)
 
-# Read in data
-water <- readRDS("data/data_4.Rds")
-water_qual <- readRDS("data/water_quality.rds")
-# https://data.cityofnewyork.us/Environment/Drinking-Water-Quality-Distribution-Monitoring-Dat/bkwf-xfky
 
-# Make map
-m <- leaflet()
-m <- addTiles(m)
-m <- setView(m, 74.0059, 40.7127, zoom = 18)
-m  # the RStudio 'headquarter'
-water_qual[water_qual$Turbidity > 1 & grepl('12', water_qual$Date)]$Turbidity
-leaflet(width="100%") %>% addProviderTiles("Stamen.TonerLite") %>% addMarkers(74.0059, 40.7127, popup="The birthplace of R") %>% setView(74.0059, 40.7127, zoom = 18)
+water_qual_Turbid <- aggregate(water_qual$Turbidity, list(water_qual$Date), mean)
+water_qual_Turbid <- rename(water_qual_Turbid, c("Group.1"="Date", "x"="Turbidity"))
+water_qual_Turbid$Date <- format(as.yearmon(water_qual_Turbid$Date, "%m/%d/%Y"), "%m")
+water_qual_Turbid <- aggregate(water_qual_Turbid$Turbidity, list(water_qual_Turbid$Date), mean)
+water_qual_Turbid <- rename(water_qual_Turbid, c("Group.1"="Date", "x"="Turbidity"))
+water_qual_Turbid$Date <- mapvalues(water_qual_Turbid$Date, from = water_qual_Turbid$Date, c("Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"))
+
+water_qual_Chlorine <- aggregate(water_qual$Chlorine, list(water_qual$Date), mean)
+water_qual_Chlorine <- rename(water_qual_Chlorine, c("Group.1"="Date", "x"="Chlorine"))
+water_qual_Chlorine$Date <- format(as.yearmon(water_qual_Chlorine$Date, "%m/%d/%Y"), "%m")
+water_qual_Chlorine <- aggregate(water_qual_Chlorine$Chlorine, list(water_qual_Chlorine$Date), mean)
+water_qual_Chlorine <- rename(water_qual_Chlorine, c("Group.1"="Date", "x"="Chlorine"))
+water_qual_Chlorine$Date <- mapvalues(water_qual_Chlorine$Date, from = water_qual_Chlorine$Date, c("Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"))
+
+rep_q_water <- quality_water
+rep_q_water <- rep_q_water[grepl("2015", rep_q_water$Date),]
+rep_q_water$Date <- (format(as.yearmon(rep_q_water$Date, "%Y-%m-%d"), "%m"))
+
+# Main data table
+rep_q_water_table <- as.data.frame(table(rep_q_water$Date, rep_q_water$Descriptor))
+rep_q_water_table <- rename(rep_q_water_table, c("Var1"="Date", "Var2"="Descriptor", "Freq"="Number"))
+
+# Total data table
+rep_q_water_table_monthly <- aggregate(rep_q_water_table$Number, list(rep_q_water_table$Date), sum)
+rep_q_water_table_monthly$Group.1 <- mapvalues(rep_q_water_table_monthly$Group.1, from = rep_q_water_table_monthly$Group.1, c("Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"))
+
+# By complaint type
+
+x_axis <- list(
+  title = "Months in 2015"
+  #range=c("Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec")
+  #titlefont = f
+)
+y_axis <- list(
+  title = "Turbidity in NTU"
+  #titlefont = f
+)
+ay <- list(
+  title="Number of Complaints",
+  overlaying = "y",
+  side = "right"
+)
+
+plot_ly(water_qual_Turbid, x =Date, y =Turbidity, name = "Turbidity Level", colors=brewer.pal(3, "BrBG"), text=paste("Turbidity:", Turbidity, " (NTU)")) %>%
+add_trace(rep_q_water_table_monthly, x=rep_q_water_table_monthly$Group.1, y = rep_q_water_table_monthly$x, name = "NYC Resident Complaints", yaxis = "y2", text=paste("Num of complaints:", rep_q_water_table_monthly$x)) %>%
+layout(xaxis = x_axis, yaxis=y_axis, yaxis2 = ay)
+
+
+#main-svg backgorund 
+# subplot xy fill
