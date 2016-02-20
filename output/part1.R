99719d927e20d933c3aa9856b8535995a585d300
cks1001652 <ycwang21@gmail.com>
2016-02-20 16:19:30 -0500

    Complaint Type
    Clean data, generate new ggplot instead of old pie3d, set function to
    calculate the data, and plot the pie chart

--- output/part1.R
+++ output/part1.R
@@ -3,13 +3,23 @@ library(data.table)
 library(ggplot2)
 library(plotrix)
 library(RColorBrewer)
-library(ggmap)
+library(scales)
 getwd()
-setwd("/Users/cheeseloveicecream/GitHub/project2-cycle2-8")
-system.time(raw <- readRDS("/Users/cheeseloveicecream/GitHub/project2-cycle2-8/data/data_4.rds"))
+system.time(raw <- readRDS("data/data_4.rds"))
 
-data <- data.frame(c(raw[,c("Complaint.Type","Incident.Zip","Incident.Address","City","Borough","Latitude","Longitude","Location")]))
-data <- tbl_df(data)
+data <- data.frame(c(raw[,c("Complaint.Type","Borough","Latitude","Longitude","Date")]))
+data_bronx <- data%>%
+  filter(Borough=='BRONX')
+data_brooklyn <-data%>%
+  filter(Borough=='BROOKLYN')
+data_manhattan <- data%>%
+  filter(Borough=='MANHATTAN')
+data_queens <- data%>%
+  filter(Borough=='QUEENS')
+data_staten_island <- data%>%
+  filter(Borough=='STATEN ISLAND')
+
+#total data
 data.total <- data%>%
   summarise(complaint.total=n())
 data.total <- rep(data.total$complaint.total,each=8)
@@ -21,25 +31,112 @@ data.count <- data%>%
 data.count <- cbind(data.count,data.total)
 data.count <- data.count%>%
   mutate(value=complaint.count/data.total*100)
-data.count[1,]
-# prepare for the pie chart
-data.count[,4] <- round(data.count[,4],digit=2)
-lbls <- c("Bottled Water: ","Drinking Water: ","Heat/Hot Water: ","Standing Water: ","Water Conservation: ","Water Leak: ","Water Quality: ","Water Quality: ")
-perlabels <- paste(lbls, data.count[,4], "%", sep="")
-slices <- data.count[,2]
-cols <- brewer.pal(8,"Set1")
-# Plot the pie chart
-par(mfrow=c(1,1)) 
-# png(filename = "figs/Eng.png", width = 680, height = 480)
-pie3D(slices, radius = 1.2, shade = 0.8, theta = 0.8, start = -0.1, border = "white",
-      col = cols, main = "Pie Chart of Complaint Types", 
-      labels = perlabels,labelrad = 1.5, labelcex = 1.3)
-#dev.copy(png, "figs/Eng.png")
-dev.off()
+#function for calculation
+calculation <- function(dataset){
+  data.total <- dataset%>%
+    summarise(complaint.total=n())
+  data.total <- rep(data.total$complaint.total,each=8)
+  
+  data.count <- dataset%>%
+    group_by(Complaint.Type)%>%
+    summarise(complaint.count=n())
+  
+  data.count <- cbind(data.count,data.total)
+  data.count <- data.count%>%
+    mutate(Proportion=complaint.count/data.total*100)
+  data.count[,4] <- round(data.count[,4],digit=2)
+  return(data.count)
+}
+#calcualtion result
+data_count <- calculation(dataset = data)
+data_bronx_count <- calculation(dataset=data_bronx)
+data_brooklyn_count <- calculation(dataset = data_brooklyn)
+data_manhattan_count <- calculation(dataset = data_manhattan)
+data_queens_count <- calculation(dataset = data_queens)
+data_staten_island_count <- calculation(dataset = data_staten_island)
+
+#function for plot
+piechart <- function(dataset){
+  dataset[,4] <- round(dataset[,4],digit=2)
+  lbls <- c("Bottled \n","Drinking \n ","Heat/Hot Water\n ","Standing\n Water\n "," Water\n Conservation\n ","Water\n Leak \n "," Water\nQuality \n ","Water System \n ")
+  perlabels <- paste(dataset[,4], "%", sep="")
+  slices <- dataset[,2]
+  cols <- brewer.pal(8,"Set1")
+  par(mfrow=c(1,1)) 
+  pie3D(slices, radius = 1.5, shade = 0.8, theta = 0.8, start = -0.1, border = "white",
+        col = cols, main = "Pie Chart of Complaint Types", 
+        labels = perlabels,labelrad = 1.2, labelcex = 1.3)
+  legend('topleft',c("Bottled Water","Drinking Water ","Heat/Hot Water ","Standing Water "," Water Conservation  ","Water Leak "," Water Quality ","Water System  "),fill=cols,cex=0.4)
+}
+piechart <- function(dataset,a1,a2,a3){
+  attach(dataset)
+  y.breaks <- cumsum(Proportion) - Proportion/2
+  p <- ggplot(dataset,aes(x=1,y=dataset[,4],fill=Complaint.Type))+
+    geom_bar(stat="identity",color='black')+
+    geom_text(aes(y= Proportion[3]*a1,label=percent(Proportion[3]/100)),size=4)+
+    geom_text(aes(y= (Proportion[6]*a2),label=percent(Proportion[6]/100)),size=4)+
+    geom_text(aes(y= (Proportion[8]*a3),label=percent(Proportion[8]/100)),size=4)+
+        ggtitle("Complaint Types Proportion")+
+    coord_polar(theta = 'y')+
+    theme(axis.ticks=element_blank(),  # the axis ticks
+          axis.title=element_blank(),  # the axis labels
+          axis.text.y=element_blank(),
+          axis.text.x=element_blank()
+#           plot.margin=rep(unit(0, "lines"),4),
+#           axis.text.x=element_text(color='black'))
+#   +
+#     scale_y_continuous(
+#       breaks=y.breaks,   # where to place the labels
+#       labels=Complaint.Type # the labels
+    )
+  
+  return(p)
+}
+#plotting
+
+piechart(dataset = data_bronx_count,a1 = 0.5,a2 = 6.7,a3=5)
+piechart(dataset = data_brooklyn_count,a1 = 0.6,a2 = 5.3,a3=3.3)
+piechart(dataset = data_manhattan_count,a1 = 0.54,a2 = 7.5,a3=3.5)
+piechart(dataset = data_queens_count,a1 = 0.5,a2 = 7.5,a3=1.77)
+piechart(dataset = data_staten_island_count,a1 = 0.5,a2 = 7,a3=1)
+
+# piechart(dataset = data.count)
+# # prepare for the pie chart
+# data.count[,4] <- round(data.count[,4],digit=2)
+# lbls <- c("Bottled Water: ","Drinking Water: ","Heat/Hot Water: ","Standing Water: ","Water Conservation: ","Water Leak: ","Water Quality: ","Water Quality: ")
+# perlabels <- paste(lbls, data.count[,4], "%", sep="")
+# slices <- data.count[,2]
+# cols <- brewer.pal(8,"Set1")
+# # Plot the pie chart
+# par(mfrow=c(1,1)) 
+# # png(filename = "figs/Eng.png", width = 680, height = 480)
+# pie3D(slices, radius = 1.2, shade = 0.8, theta = 0.8, start = -0.1, border = "white",
+#       col = cols, main = "Pie Chart of Complaint Types", 
+#       labels = perlabels,labelrad = 1.5, labelcex = 1.3)
+# #dev.copy(png, "figs/Eng.png")
+# dev.off()
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #histogram
 # Prepare for the histogram/bar
-hist <- raw[,c("Complaint.Type","Borough","Incident.Zip","City")]
+hist <- raw[,c("Complaint.Type","Borough")]
 # Histogram plot one 
 p1 <- ggplot(hist,aes(Borough,fill=Complaint.Type))  
 p1 + geom_bar(position="stack")+
@@ -47,61 +144,4 @@ p1 + geom_bar(position="stack")+
   xlab("Borough")+
   ylab("Numbers of Complaints")
 
-# Histogram plot two
-# p2 <- ggplot(hist,aes(City,fill=Complaint.Type))  
-# p2 + geom_bar(position="stack")+
-#   ggtitle("Different Complaints in Different City")+
-#   xlab("City")+
-#   ylab("Numbers of Complaints")
 
-#map
-houston <- get_map("new york", zoom = 13)
-HoustonMap <- ggmap(houston, extent = "device", legend = "topleft")
-HoustonMap +
-  stat_density2d(
-    aes(x = lon, y = lat, fill = ..level..,  alpha = ..level..),
-    size = 2,  data = violent_crimes,
-    geom = "polygon"
-  )
-overlay <- stat_density2d(
-  aes(x = lon, y = lat, fill = ..level.., alpha = ..level..),
-   geom = "polygon",
-  data = violent_crimes
-)
-HoustonMap + overlay + inset(
-  grob = ggplotGrob(ggplot() + overlay + theme_inset()),
-  xmin = -95.35836, xmax = Inf, ymin = -Inf, ymax = 29.75062
-)
-
-qmap('houston', zoom = 13)
-
-
- gglocator(2)
-# only violent crimes
-violent_crimes <- subset(crime,offense != "auto theft" & offense != "theft" & offense != "burglary")
-# order violent crimes
-violent_crimes$offense <- factor(violent_crimes$offense,levels = c("robbery", "aggravated assault", "rape", "murder"))
-# restrict to downtown
-violent_crimes <- subset(violent_crimes,-95.39681 <= lon & lon <= -95.34188 &29.73631 <= lat & lat <=  29.78400)
-theme_set(theme_bw(16))
-HoustonMap <- qmap("houston", zoom = 14, color = "bw", legend = "topleft")
-HoustonMap +
-  geom_point(aes(x = lon, y = lat, colour = offense, size = offense),
-             data = violent_crimes)
-HoustonMap +
-  stat_bin2d(
-    aes(x = lon, y = lat, colour = offense, fill = offense),
-    size = .5, bins = 30, alpha = 1/2,
-    data = violent_crimes
-  )
-
-
-houston <- get_map(location = "houston", zoom = 14, color = "bw",
-                   source = "osm")
-HoustonMap <- ggmap(houston, base_layer = ggplot(aes(x = lon, y = lat),
-                                                 data = violent_crimes))
-HoustonMap +
-  stat_density2d(aes(x = lon, y = lat, fill = ..level.., alpha = ..level..), geom = "polygon",
-                 data = violent_crimes) +
-  scale_fill_gradient(low = "black", high = "red") +
-  facet_wrap(~ day)
